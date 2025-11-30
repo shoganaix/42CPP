faa0b0c137603476bf1381cc3d1269fc954123ad
Maria Soriano <msoriano@c2r6s1.42madrid.com>
2025-11-30 20:59:05 +0100

    Module08 ex01 wip

--- Module08/ex01/Span.cpp
+++ Module08/ex01/Span.cpp
@@ -3,10 +3,87 @@
 /*                                                        :::      ::::::::   */
 /*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: usuario <usuario@student.42.fr>            +#+  +:+       +#+        */
+/*   By: msoriano <msoriano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 02:33:00 by usuario           #+#    #+#             */
-/*   Updated: 2025/11/30 02:33:00 by usuario          ###   ########.fr       */
+/*   Updated: 2025/11/30 20:58:14 by msoriano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Span.hpp"
+
+Span::Span()
+{
+    this->N = 0;
+    this-> data = null;
+}
+
+Span::Span(unsigned int N1)
+{
+    this->N = N1;
+    this-> data = null;
+}
+
+Span::Span(const Span &other)
+{
+    this->N = other.N;
+    this->data = other.data;
+}
+
+Span::~Span()
+{
+
+}
+
+Span &Span::operator=(const Span &other)
+{
+    if (this != &other)
+    {
+        this->N = other.N;
+        this->data = other.data;
+    }
+    return (*this);
+}
+
+void Span::addNumber(int nb)
+{
+    if (data.size() >= N)
+        throw FullSpan();
+    data.push_back(nb);
+}
+
+template <typename It> void Span::addMultipleNumbers(It begin, It end)
+{
+
+}
+
+
+int Span::shortestSpan() const
+{
+    if (data.size() < 2)
+        throw EmptySpan();
+
+    std::vector<int> sorted = data;                     // Create copy container 
+    std::sort(sorted.begin(), sorted.end());            // Sort numerically this container
+
+    int minSpan = sorted[1] - sorted[0];                // We set minSpan to the diff between the first two nb
+    for (size_t i = 1; i < sorted.size() - 1; ++i)      // We update minSpan if smaller
+    {
+        int diff = sorted[i + 1] - sorted[i];
+        if (diff < minSpan)
+            minSpan = diff;
+    }
+    return (minSpan);
+}
+
+int Span::longestSpan() const
+{
+    if (data.size() < 2)
+        throw EmptySpan();
+
+    int minVal = *std::min_element(data.begin(), data.end());
+    int maxVal = *std::max_element(data.begin(), data.end());
+
+    return (maxVal - minVal);
+}
 
--- Module08/ex01/Span.hpp
+++ Module08/ex01/Span.hpp
@@ -3,10 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: usuario <usuario@student.42.fr>            +#+  +:+       +#+        */
+/*   By: msoriano <msoriano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 02:32:57 by usuario           #+#    #+#             */
-/*   Updated: 2025/11/30 02:32:58 by usuario          ###   ########.fr       */
+/*   Updated: 2025/11/30 20:03:22 by msoriano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <vector>
+#include <algorithm>
+#include <exception>
+#include <iostream>
+
+
+class Span
+{
+    private:
+        unsigned int N;
+        std::vector<int> data; 
+        // We use vector because its a dynamic STL container that store elements contiguous memory
+        //      - It grows automatically (doesn't know beforehand N value, array container IMPOSSIBLE)
+        //      - No need to allocate with new[]
+        //      - Built-in algorithms (sort, min, max...)
+
+    public:
+        Span();
+        Span(unsigned int N1);
+        Span(const Span &other);
+        ~Span();
+
+        Span &operator=(const Span &other);
+
+        void addNumber(int nb);
+        
+        // void addMultipleNumbers(int amount); SUBJECT SPECIFICALLY ASKS TO FILL SPAN USING A RANGE OF ITERATORS
+
+        template <typename It> void addMultipleNumbers(It begin, It end);
+        // We create a template because it has to accept any type of ::iterators (list, array, vector...)
+                                /*                                                  *
+                                * Span sp(5);                                       *
+                                * std::deque<int> d = {100, 200, 300};              *
+                                * sp.addMultipleNumbers(d.begin(), d.end());        *
+                                *                                                   *
+                                * sp.data = [100, 200, 300];                        *
+                                *                                                   */
+
+        int shortestSpan() const;
+        // Returns the smallest difference between ANY two stored numbers (requires two numbers on data) -> throws exception
+
+        int longestSpan() const;
+        // Returns the largest difference between ANY two stored numbers (requires two numbers on data) -> throws exception
+
+};
+
+class FullSpan : public std::exception
+{
+    public:
+        const char *what() const throw() 
+        {
+            return ("[EXCEPTION] Container is full: N elements already stored");
+        }
+};
+
+class EmptySpan : public std::exception
+{
+    public:
+        const char *what() const throw() 
+        {
+            return ("[EXCEPTION] Span couldn't be found: Not enough numbers");
+        }
+};
+
+#endif
