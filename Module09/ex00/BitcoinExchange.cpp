7172a1995e0281f1e37bc0a749fb6812d0a2da6b
Marichu PC <meryetc@gmail.com>
2025-12-03 17:26:05 +0100

    ex00 done

--- Module09/ex00/BitcoinExchange.cpp
+++ Module09/ex00/BitcoinExchange.cpp
@@ -3,25 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msoriano <msoriano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: usuario <usuario@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:58:20 by msoriano          #+#    #+#             */
-/*   Updated: 2025/12/02 19:06:47 by msoriano         ###   ########.fr       */
+/*   Updated: 2025/12/03 17:25:46 by usuario          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-// A leap year is divisible by 4, but not by 100, unless it is also divisible by 400
-// Example: 
-//          2024 is a leap year because it's divisible by 4.
-//          1900 was not a leap year because it is divisible by 100 but not by 400
-
+/*
+    A leap year is divisible by 4, but not by 100, unless it is also divisible by 400
+     Example: 
+        2024 is a leap year because it's divisible by 4.
+        1900 was not a leap year because it is divisible by 100 but not by 400
+*/
 bool isLeapYear(int year)
 {
     return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 }
 
+/*
+    "A valid date will always be in the following format: Year-Month-Day"
+    We also check that the date exists (feb 30th NOT real)
+*/
 bool BitcoinExchange::validDate(const std::string &date) const
 {
     if (date.size() != 10 || date[4] != '-' || date[7] != '-') // [....-..-..]
@@ -43,51 +48,72 @@ bool BitcoinExchange::validDate(const std::string &date) const
     return (true);
 }
 
-bool BitcoinExchange::validFormatAndValue(const std::string &str, double &value) const
+/*
+    A valid value must be either a float or a positive integer, between 0 and 1000
+*/
+bool BitcoinExchange::validValue(const std::string &valueStr, double &value) const
 {
-    if (!isInt(value) || !isFloat(value))
+    // Convert str to double
+    value = std::atof(valueStr.c_str());
+    
+    // Is it empty?
+    if (valueStr.empty())
+        return false;
+
+    if (value < 0 || value > 1000)
         return (false);
-    if (value < 0)
-        return (false);
-    if (value > 1000)
-        return (false);
+
+    //Is it digit, + or .?
+    for (size_t i = 0; i < valueStr.size(); ++i)
+    {
+        if (!isdigit(valueStr[i]) && valueStr[i] != '.' && valueStr[i] != '+')
+            return false;
+    }
+
     return (true);
 }
 
-void BitcoinExchange::loadDatabase(const std::string &filename)
-{
 
-}
-
-BitcoinExchange::BitcoinExchange(): {
-}
-
-
+/*
+    "The program name is btc.
+     This program must use a database in csv format"
+*/
 void BitcoinExchange::loadDatabase(const std::string &filename)
 {
     std::ifstream file(filename.c_str());
-    if (!file.is_open())
-        throw InvalidFormat();
 
+    if (!file.is_open())
+        throw FileNotFound();
+    
     std::string line;
-    std::getline(file, line); // saltar header
+
+    // Skip header ("date,exchange_rate")
+    std::getline(file, line);
 
     while (std::getline(file, line))
     {
         std::stringstream ss(line);
         std::string date, valueStr;
+
+        // Must be separated by ',' ?
         if (!std::getline(ss, date, ',') || !std::getline(ss, valueStr))
             continue;
 
-        double value;
-        if (!validFormatAndValue(valueStr, value))
-            throw InvalidValue();
+        // Trim spaces
+        date.erase(0, date.find_first_not_of(" "));
+        date.erase(date.find_last_not_of(" ") + 1);
+        valueStr.erase(0, valueStr.find_first_not_of(" "));
+        valueStr.erase(valueStr.find_last_not_of(" ") + 1);
 
-        if (!validDate(date))
-            throw InvalidDate();
+        double value;
+        if (!validDate(date) || !validValue(valueStr, value))
+            continue;
 
         database[date] = value;
     }
+}
+
+BitcoinExchange::BitcoinExchange() {
 }
 
 BitcoinExchange::BitcoinExchange(const char *file)
@@ -110,16 +136,34 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
 BitcoinExchange::~BitcoinExchange() {
 }
 
-double BitcoinExchange::getValue() const
-{
-    iterator it = database.lower_bound(date);
 
+/*
+    "If the date used in the input does not exist in your DB then you 
+        must use the closest date contained in your DB. 
+     Be careful to use the lower date and not the upper one."
+
+    MEANING IF DATE NOT EXACT ALWAYS RETURN THE EALIER DATE !
+*/
+double BitcoinExchange::getValue(const std::string &date) const
+{
+/*  
+    auto it = database.lower_bound(date);
+
+    C++ wont let us use 'auto' keyword which automatically deduces the type of iterator:
+        - Shorter than  writing 'std::map<std::string, double>::const_iterator' =
+        - Works even if the map type changes or the method is const
+    lower_bound() returns an iterator to the first element >= date
+*/
+    std::map<std::string, double>::const_iterator it = database.lower_bound(date);
+
+    //There are no dates >=  or it->date is not = date
     if (it == database.end() || it->first != date)
     {
+        // If it points to first element there is no earlier date
         if (it == database.begin())
-            throw InvalidDate();
+                return (0);
         --it;
     }
-
+    // it->first is the date (key) | it->second the value 
     return it->second;
 }
--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -3,16 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msoriano <msoriano@student.42.fr>          +#+  +:+       +#+        */
+/*   By: usuario <usuario@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 16:55:58 by msoriano          #+#    #+#             */
-/*   Updated: 2025/12/02 18:58:57 by msoriano         ###   ########.fr       */
+/*   Updated: 2025/12/03 17:24:07 by usuario          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+    // "Your program must take a file as an argument"
+    if (argc != 2)
+    {
+        std::cerr << "\033[31m[Error]:\033[0m Program must be called with one file: ./btc <input.txt>" << std::endl;
+        return (1);
+    }
+
+    try
+    {
+        BitcoinExchange btc("data.csv");
+        
+        std::ifstream infile(argv[1]);
+        if (!infile.is_open())
+        {
+            std::cerr << "\033[31m[Error]:\033[0m Could not open input file" << std::endl;
+            return (1);
+        }
+
+        std::string line;
+        
+        std::getline(infile, line);
+        line.erase(0, line.find_first_not_of(" \t"));
+        line.erase(line.find_last_not_of(" \t") + 1);
+
+        if (line != "date | value")
+            std::cout << "\033[31m[Error]:\033[0m Bad header => " << line << std::endl;
+        
+        while (std::getline(infile, line))
+        {
+            // Ignore new lines
+            if (line.empty())
+                continue;
+
+            std::stringstream ss(line);
+            std::string date, valueStr;
+
+            // Must be separated by | -> (relocated from loadDatebase())
+            if (!std::getline(ss, date, '|') || !std::getline(ss, valueStr))
+            {
+                std::cout << "\033[31m[Error]:\033[0m Bad input => " << line << std::endl;
+                continue;
+            }
+
+            // Trim spaces (relocated from loadDatebase())
+            date.erase(0, date.find_first_not_of(" "));
+            date.erase(date.find_last_not_of(" ") + 1);
+            valueStr.erase(0, valueStr.find_first_not_of(" "));
+            valueStr.erase(valueStr.find_last_not_of(" ") + 1);
+
+            double value;
+            // A valid value must be either a float or a positive integer, between 0 and 1000.
+            if (!btc.validValue(valueStr, value)) 
+            {
+                std::cout << "\033[31m[Error]:\033[0m Not a valid number." << std::endl;
+                continue;
+            }
+            
+            // A valid date will always be in the following format: Year-Month-Day.
+            if (!btc.validDate(date)) 
+            {
+                std::cout << "\033[31m[Error]:\033[0m Not a valid date." << std::endl;
+                continue;
+            }
+            double rate = btc.getValue(date);
+            double result = value * rate;
+            std::cout << date << " => " << value << " = "
+                      << std::fixed << std::setprecision(1) << result << std::endl;
+        }
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return (1);
+    }
+
     return (0);
 }
